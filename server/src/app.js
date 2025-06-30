9b17cd39114bb8f836e349ca54848504739a9062
Gyemfa-Gloria-Abedi <140189493+Gyemfa-Gloria-Abedi@users.noreply.github.com>
2025-06-30 16:07:20 +0000

    initialized express server

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan("combined")); 
+app.use(bodyParser.json()); //allows json files to be parsed into sentence
+app.use(cors()); //allows clients to access server using http/https
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: "Hello world"
+    });
+})
+app.listen(process.env.PORT || 8081);
